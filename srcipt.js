9defd311675f76d15ca630739e2b77036057b2c2
Leah <leahxharden@gmail.com>
2023-02-24 16:41:55 -0500

    added basic files

---
+++ srcipt.js
@@ -0,0 +1,49 @@
+const inquirer = require('inquirer');
+
+const fs = require('fs');
+
+
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+        validate()
+        },
+        {
+        type: 'checkbox',
+        message: 'What languages do you know?',
+        name: 'stack',
+        choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+        },
+        {
+        type: 'list',
+        message: 'What is your preferred method of communication?',
+        name: 'contact',
+        choices: ['email', 'phone', 'telekinesis'],
+        },
+    ]
+    
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+
+    .then((data) => {
+        const Code = `  `
+}
+}
+// Function call to initialize app
+init();
+
+.then((data) => {
+
+    fs.writeFile(README.md, JSON.stringify(data, null, '\t'), (err) =>
+    err ? console.log(err) : console.log('Success!')
+    );
+});
