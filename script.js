b70ef07578f873e33bfa5081db13b4e6e5b93a7c
Leah <leahxharden@gmail.com>
2023-02-28 20:16:37 -0500

    fixing badges still /WIP

--- script.js
+++ script.js
@@ -151,13 +151,12 @@ ${questions.questions_email}
 
 
 
-
 function licenseBadge(answers) {
-    let licenseBadgevar;
+    let licenseBadgeVar;
     let licenseDescription;
     
     if (answers.license_badge === 'MIT') {
-        licenseBadgevar = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+        licenseBadgeVar = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
         licenseDescription = `Permission is hereby granted, free of charge,
         to any person obtaining a copy of this software and associated documentation 
         files (the “Software”), to deal in the Software without restriction, including
@@ -165,7 +164,7 @@ function licenseBadge(answers) {
         sublicense, and/or sell copies of the Software, and to permit persons to whom the 
         Software is furnished to do so, subject to the following conditions:.`
     } else if (answers.license_badge === 'Boost') {
-        licenseBadgevar = `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
+        licenseBadgeVar = `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
         licenseDescription = `Permission is hereby granted, free of charge, to any person or organization
         obtaining a copy of the software and accompanying documentation covered by
         this license (the "Software") to use, reproduce, display, distribute,
@@ -173,7 +172,7 @@ function licenseBadge(answers) {
         Software, and to permit third-parties to whom the Software is furnished to
         do so, all subject to the following:`;
     } else if (answers.license_badge === 'Mozilla Public License 2.0') {
-        licenseBadgevar = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+        licenseBadgeVar = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
         licenseDescription = `Each Contributor hereby grants You a world-wide, royalty-free, non-exclusive license:
         under intellectual property rights (other than patent or trademark) Licensable
         by such Contributor to use, reproduce, make available, modify, display, perform, distribute, 
@@ -181,15 +180,17 @@ function licenseBadge(answers) {
         under Patent Claims of such Contributor to make, use, sell, offer for sale, have made, import, 
         and otherwise transfer either its Contributions or its Contributor Version.`;
     } else {
-        licenseBadgevar = ''; // no license badge
+        licenseBadgeVar = ''; // no license badge
         licenseDescription = 'Default copyright laws apply.';
     }
+    return [ licenseBadgeVar, licenseDescription ]
+    
+    
 }
 function writefile(fileName, answers) {
-    //fs.writeFile(README.md, generateReadMe(questions), (err) =>
     data = generateReadMe(answers)
     fs.writeFile(fileName, data, (err) =>
-        err ? console.log(err) : console.log('Success!'),
+    err ? console.log(err) : console.log('Success!'),
     );
 }
 // TODO: Create a function to initialize app
@@ -200,12 +201,16 @@ function init() {
             .prompt(questions)
             .then((answers) => {
                 //console.log(answers, "answers"),
-                    licenseBadge(answers)
+                licenseBadge(answers)
+                const badgesData = licenseBadge(answers);
+                // Answers = { item: answers }
+                // Answers.newItem = "value"; 
+                
                 //    .then((response) => {
-                //        concat(answers,response)
+                    //        concat(answers,response)
                     writefile(fileName, answers)
-                //    })
-            })
+                    //    })
+                })
     // TODO: Create a function to write README file
 }
 // Function call to initialize app
